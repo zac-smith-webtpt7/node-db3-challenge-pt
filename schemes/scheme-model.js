2595a737d90190b86582bd409d5d3ab0b85ce6e9
Zac Smith <zrs3141592@gmail.com>
2019-11-19 21:00:32 -0700

    models are work MVP complete

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -20,19 +20,24 @@ function findById(id) {
 }
 
 function findSteps(id) {
-  return db("steps")
-    .join("schemes, schemes.id, steps.scheme_id")
-    .select("schemes.scheme_name", "steps.step_number", "steps.instructions")
-    .where({ scheme_id: id })
+  return db
+    .select(
+      "steps.id",
+      "steps.step_number",
+      "steps.instructions",
+      "schemes.scheme_name"
+    )
+    .from("steps")
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .where("steps.scheme_id", id)
     .orderBy("steps.step_number");
 }
 
 function add(scheme) {
+  const { scheme_name } = scheme;
   return db("schemes")
-    .insert(scheme)
-    .then(ids => {
-      return findById(ids[0]);
-    });
+    .insert({ scheme_name })
+    .then(([id]) => findById(id));
 }
 
 function update(changes, id) {
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const SchemeRouter = require("./schemes/scheme-router.js");
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use("/api/schemes", SchemeRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    message: "GET / is working"
+  });
+});
 
 module.exports = server;
